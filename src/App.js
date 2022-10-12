3d6390fa0faf5fcf3a641741c277573c0109f00a
Tarik <a.tarik2005@gmail.com>
2022-10-13 02:05:54 +0300

    card tamam

--- src/App.js
+++ src/App.js
@@ -2,16 +2,42 @@ import logo from './logo.svg';
 import './App.css';
 import Card from './components/card/Card';
 import data from './data';
+
+import './components/card/Card.css'
+import { useState } from 'react';
+
+import { FaChevronLeft, FaChevronRight } from "react-icons/fa";
+
 function App() {
+  const [count,setCount]=useState(0);
+
+  const prev =()=>{
+    if(count >0 ){setCount(count -5)}
+    if(count <4){setCount(15)}
+   }
+   
+const next =()=>{
+  if(count <15){setCount(count +5)}
+  if(count>14){setCount(0)}
+ }
+ 
+
+
+
   return (
-    <div className="App">
-    <h2>Employye List</h2>
+    <div className="container text-center">
+    <h1>Employye List</h1>
+    <h4>(Employees {count +1} to {count +5})</h4>
+    
     <div>
-      {data.map((item)=>{
+      {data.slice(count,count + 5).map((item)=>{
        return <Card {...item} key={item.id} />
       })}
     </div>
-
+      <div className='button-style '>
+      <span className="mx-3">
+          <FaChevronLeft onClick={prev} />Prev</span>  
+    <span className='mx-3'>Next<FaChevronRight onClick={next} /></span></div>
 
 
     </div>
--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -1,15 +1,16 @@
 import React from 'react'
-import { FaChevronLeft, FaChevronRight } from "react-icons/fa";
+import './Card.css'
 const Cards = ({id,name,age,image,email}) => {
   
   return ( 
-    <div>
+    <div className='card_style'>
+       <img src={image} alt="" />
+       <div className='card_text'>
         <div>{name}</div>
         <div>{age}</div>
-        <img src={image} alt="" />
+       
         <div>{email}</div>
-        <span><FaChevronLeft/></span>
-        <span><FaChevronRight/></span>
+        </div>
         </div>
   )
 }
